00a9518a2c1a99807c389207179fd99ba873dda1
filipljung <96741967+filipljung@users.noreply.github.com>
2023-06-25 20:30:52 +0600

    Update urls.py

--- pinterest/urls.py
+++ pinterest/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import home, search
+from .views import *
 
 app_name = 'pinterest'
 
 urlpatterns = [
     path('', home, name='home'),
     path('search/', search, name='search'),
+    path('about/', about, name='about'),
+    path('business/', business, name='business'),
+    path('blog/', blog, name='blog'),
 ]
